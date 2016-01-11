1210cd96b16ddadb244bb2aaf03d7d5bc21d46ce
Paul Traylor <kungfudiscomonkey@gmail.com>
2016-01-11 15:30:56 +0900

    Filter out some possibly inactive projects

--- _data/tags.py
+++ _data/tags.py
@@ -1,6 +1,9 @@
 # http get 'https://api.github.com/users/kfdm/repos?per_page=100' > github.api.json
+
+import datetime
+import dateutil.parser
+import dateutil.tz
 import json
-import collections
 import yaml
 
 GROUPS = [
@@ -12,8 +15,15 @@ GROUPS = [
 ]
 FORKS = ['growlme']
 
+# Inactive for ~3 years
+INACTIVE = 60 * 60 * 24 * 365 * 3
+
+now = datetime.datetime.now(dateutil.tz.tzlocal())
+
 projects = {}
 untagged = {}
+inactive = {}
+
 for repo in json.loads(open('github.api.json').read()):
     if repo['fork'] and repo['name'] not in FORKS:
         continue
@@ -42,15 +52,23 @@ for repo in json.loads(open('github.api.json').read()):
     if repo['description']:
         project['description'] = repo['description']
 
+    pushed_at = dateutil.parser.parse(repo['pushed_at'])
+    if (now - pushed_at).total_seconds() > INACTIVE:
+        project['tags'].add('inactive')
+
     project['tags'] = list(project['tags'])
-    if project['tags']:
+    if 'inactive' in project['tags']:
+        inactive[repo['full_name']] = project
+    elif project['tags']:
         projects[repo['full_name']] = project
     else:
         untagged[repo['full_name']] = project
-        continue
+
+
 
     if repo['language']:
         project['language'] = repo['language']
 
-open('tags.yaml', 'w+').write(yaml.safe_dump(projects, indent=4, line_break=False))
-open('untagged.yaml', 'w+').write(yaml.safe_dump(untagged, indent=4, line_break=False))
+open('projects/tags.yaml', 'w+').write(yaml.safe_dump(projects, indent=4, line_break=False))
+open('projects/untagged.yaml', 'w+').write(yaml.safe_dump(untagged, indent=4, line_break=False))
+open('projects/inactive.yaml', 'w+').write(yaml.safe_dump(inactive, indent=4, line_break=False))
